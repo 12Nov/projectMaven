e17cf6aa2468c8bfb15bcfbb3d9df6bf864ca7a3
SoftwareTesting12 <manjushasutar58@gmail.com>
2023-07-31 13:49:46 +0530

    [Manjusha] Commiting Ajio code.

---
+++ src/test/java/ecommerce/Ajio.java
@@ -0,0 +1,16 @@
+package ecommerce;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Ajio {
+  @Test
+  public void ajioTest() throws InterruptedException {
+	  WebDriver driver= new ChromeDriver();
+	  driver.get("https://www.ajio.com/");
+	 
+	  Thread.sleep(5000);
+	  driver.close();
+  }
+}
